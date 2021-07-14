75dbce779278358880fa1de7cf205466246dcff0
Carpetcruiser <59855241+Carpetcruiser@users.noreply.github.com>
2021-07-14 15:48:54 -0700

    Search kinda going (I think)

--- Brenparsing.js
+++ Brenparsing.js
@@ -24,12 +24,9 @@ class User {
 
 }
 
-const names = require('./names.json');
+const users = require('./names.json');
 
-users = [];
-
-users = JSON.parse(names);
-
+console.log("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
 class Parser {
 
    // // return an array of values that match on a certain key
@@ -60,32 +57,65 @@ class Parser {
       //parse string of incoming words forming email body into "tokens"
       wordArray = str.split(" ");
 
+      emailBodySearch(wordArray);
+
+
+   }
+
+   emailBodySearch(wordArray) {
       // for each word in email, search
       // if current "for each" loop is true (i.e. database does contain user)
       for (let i = 1; i < wordArray.length; i++) {
 
          // search database for word left and right of found word
          // if current array index is in database 
-         if (searchDatabase(users, wordArray[i])) {
+         if (searchDatabaseFirst(wordArray[i])) {
 
             // initialize bool tracker 
             currentSearch = false;
 
             // if first/last name is in database 
-            if (searchDatabase(wordArray[i - 1])) {
+            if (users[i - 1].lastName.toLowerCase() == wordArray[i-1].toLowerCase()) {
 
             }
-            // if also true, then name is found 
-
-            // create user with data 
-
          }
       }
-
    }
 
-   searchDatabase(users, firstName) {
+   // search for first name 
+   searchDatabaseFirst(firstNameFind) {
+      for (let people = 1; people < users.length; people++) {
+         if (users[people].firstName.toLowerCase() === firstNameFind.toLowerCase()) {
+            return true;
+         }
+      }
+      return false;
+   }
 
+   // search for last name 
+   searchDatabaseLast(lastNameFind) {
+      for (let people = 1; people < users.length; people++) {
+         if (users[people].lastName.toLowerCase() === lastNameFind.toLowerCase()) {
+            return true;
+         }
+      }
+      return false;
+   }
+
+   // search full name 
+   searchDatabase(firstNameFind, lastNameFind) {
+
+      // for each person in array 
+      for (let people = 1; people < users.length; people++) {
+
+         // if first and last name match index' first and last name 
+         if (users[people].firstName.toLowerCase() === firstNameFind.toLowerCase() && users[people].lastName.toLowerCase() === lastNameFind.toLowerCase()) {
+
+            // return true 
+            return true;
+         }
+      }
+      return false;
    }
 }
 
