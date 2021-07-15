0484007cd8e4e615ed47d2282b7a003af11939b2
Carpetcruiser <59855241+Carpetcruiser@users.noreply.github.com>
2021-07-15 11:04:47 -0700

    Search working but not duplicate names

--- Brenparsing.js
+++ Brenparsing.js
@@ -7,15 +7,12 @@
  * 
  * use those tokens to test against database
  * 
- * if found
- *    contain and return the user as an object to be used for the front end
- * 
- * if not found 
- *    return null object so front end knows to display nothing 
+ * Returns an array of any found users (i.e. if no users are found will be empty array)
  */
 class User {
 
    constructor(firstName, lastName, preferred_name, eMail, pronouns) {
+      // needs to be an array 
       this.firstName = firstName;
       this.lastName = lastName;
       this.preferred_name = preferred_name;
@@ -25,91 +22,128 @@ class User {
 
 }
 
-const users = require('./names.json');
+const usersJS = require('./names.json');
+foundPeopleArray = [];
+users = [];
 
-console.log("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
 
-class Parser {
+function javascriptArrayAdapting() {
+   // for loop creating user objects 
+   for (let key in usersJS) {
+      for (let i = 0; i < usersJS[key].length; i++) {
+         const person = new User(usersJS[key][i].first_name, key, usersJS[key][i].preferred_name, usersJS[key][i].email, usersJS[key][i].pronouns);
+         users.push(person);
 
-   // parse method
-   parse(incomingEmail) {
-
-      //parse string of incoming words forming email body into "tokens"
-      wordArray = str.split(" ");
-
-      emailBodySearch(wordArray);
-
+      }
 
    }
+}
 
-   emailBodySearch(wordArray) {
-
-      foundPeopleArray = [];
-      
-      // for each word in email, search
-      // if current "for each" loop is true (i.e. database does contain user)
-      for (let i = 1; i < wordArray.length; i++) {
-
-         // search database for word left and right of found word
-         // if current array index is in database 
-         if (searchDatabaseFirst(wordArray[i])) {
-
-            // initialize bool tracker 
-            currentSearch = false;
-
-            // if first/last name is in database 
-            if (users[i - 1].lastName.toLowerCase() == wordArray[i-1].toLowerCase()) {
-
-               // then in last, first format 
-               let foundPeopleArray = users[]
-            }
-         }
+// search for first name 
+function searchDatabaseFirst(firstNameFind) {
+   for (let people = 1; people < users.length; people++) {
+      if (users[people].firstName.toLowerCase() === firstNameFind.toLowerCase()) {
+         return true;
       }
    }
+   return false;
+}
 
-   // search for first name 
-   searchDatabaseFirst(firstNameFind) {
-      for (let people = 1; people < users.length; people++) {
-         if (users[people].firstName.toLowerCase() === firstNameFind.toLowerCase()) {
-            return true;
-         }
+
+// search for last name in database
+// takes last name to be searched for 
+function searchDatabaseLast(lastNameFind) {
+   // for each person in array
+   for (let people = 1; people < users.length; people++) {
+      // if current person's last name matches last name to be found 
+      if (users[people].lastName.toLowerCase() === lastNameFind.toLowerCase()) {
+         // return true 
+         return true;
       }
-      return false;
    }
+   // if nothing found, return false 
+   return false;
+}
 
-   searchDatabaseFirstIndex(firstNameFind) {
-      for (let people = 1; people < users.length; people++) {
-         if (users[people].firstName.toLowerCase() === firstNameFind.toLowerCase()) {
-            return true;
-         }
-      }
-      return false;
-   }
 
-   // search for last name 
-   searchDatabaseLast(lastNameFind) {
-      for (let people = 1; people < users.length; people++) {
-         if (users[people].lastName.toLowerCase() === lastNameFind.toLowerCase()) {
-            return true;
-         }
-      }
-      return false;
-   }
+// search full name with first and last name 
+function searchDatabase(firstNameFind, lastNameFind) {
 
-   // search full name 
-   searchDatabase(firstNameFind, lastNameFind) {
-
-      // for each person in array 
-      for (let people = 1; people < users.length; people++) {
+   // for each person in array 
+   for (let people = 1; people < users.length; people++) {
+      for (let firstNameIndex = 0; firstNameIndex < users[people].firstName.length; firstNameIndex++) {
 
          // if first and last name match index' first and last name 
-         if (users[people].firstName.toLowerCase() === firstNameFind.toLowerCase() && users[people].lastName.toLowerCase() === lastNameFind.toLowerCase()) {
+         if (users[people].firstName[firstNameIndex].toLowerCase() === firstNameFind.toLowerCase() && users[people].lastName.toLowerCase() === lastNameFind.toLowerCase()) {
 
             // return true 
             return true;
          }
       }
-      return false;
+   }
+   return false;
+}
+
+// same functionality as searchdatabase but returns the object of the user we're looking for 
+function searchDatabaseIndex(firstNameFind, lastNameFind) {
+
+   // for each person in array 
+   for (let people = 1; people < users.length; people++) {
+
+      for (let firstNameIndex = 0; firstNameIndex < users[people].firstName.length; firstNameIndex++) {
+         // if first and last name match index' first and last name 
+         if (users[people].firstName[firstNameIndex].toLowerCase() === firstNameFind.toLowerCase() && users[people].lastName.toLowerCase() === lastNameFind.toLowerCase()) {
+
+            // return true 
+            return users[people];
+         }
+      }
+   }
+}
+// searches the body of the email per tokenized word to reference with the imported .json file objects
+function emailBodySearch(wordArray) {
+
+   foundPeopleArray = [];
+
+   // for each word in email, search
+   // if current "for each" loop is true (i.e. database does contain user)
+   for (let i = 1; i < wordArray.length; i++) {
+
+      // search database for word left and right of found word
+      // if current array index is in database 
+      if (searchDatabaseLast(wordArray[i])) {
+
+         // initialize bool tracker 
+         currentSearch = false;
+
+         // if first/last or last/first name is in database 
+         if (searchDatabase(wordArray[i], wordArray[i - 1])) {
+            foundPeopleArray.push(searchDatabaseIndex(wordArray[i], wordArray[i - 1]));
+            // then in last, first format 
+
+         }
+         // if database has object with names in this order
+         if (searchDatabase(wordArray[i - 1], wordArray[i])) {
+            // append found user to end of foundPeopleArray
+            foundPeopleArray.push(searchDatabaseIndex(wordArray[i - 1], wordArray[i]));
+         }
+      }
    }
 }
 
+// parse method
+function parse(incomingEmail) {
+
+   //parse string of incoming words forming email body into "tokens"
+   wordArray = incomingEmail.split(" ");
+
+   emailBodySearch(wordArray);
+
+}
+
+javascriptArrayAdapting();
+console.log("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
+parse("hi alex matthews i just noticed that we have like people named andrew cox could you let them all know that we want to have an andrew cox convention i thought we could get them all together and have them compete to see who s the best one of them always signs their name like this cox drew at the end of their emails and it always grinds my gears i hope he loses anyways thanks bill");
+
+
+console.log(foundPeopleArray[0]);
